4fe4ce2daf0041b5a7b2e78ad4a5de9c5f13d20c
mawarp3k <74587423+mawarp3k@users.noreply.github.com>
2020-11-17 14:18:20 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -2,4 +2,10 @@
 
 include_once("mawar.html");
 
+echo '<script type="text/JavaScript">  
+     prompt("GeeksForGeeks"); 
+     </script>' 
+; 
+
+<link rel="stylesheet" type="text/css" href="style.css">
 ?>
